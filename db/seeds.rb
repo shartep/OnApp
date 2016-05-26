819abed099957c7077a359f58d0bad63c73478a9
shartep <shartep@mail.ru>
2016-05-26 20:29:39 +0300

    setting up root route, remove unused routes, generate schema for DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Department.create name: 'Main Department'
+Department.create name: 'First Department'
+Department.create name: 'Second Department'
