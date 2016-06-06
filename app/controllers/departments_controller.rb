8be4e375becd7522e9f8898895268d8e65de110b
Alex Petrash <alexp@elephantheadsoft.com>
2016-06-06 19:36:24 +0300

    regenerate user scaffold with using responders generator

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -1,54 +1,44 @@
 class DepartmentsController < ApplicationController
   before_action :set_department, only: [:show, :edit, :update, :destroy]
 
-  # GET /departments
-  # GET /departments.json
   def index
     @departments = Department.all
   end
 
-  # GET /departments/1
-  # GET /departments/1.json
   def show
-  end
-
-  # GET /departments/new
-  def new
-    @department = Department.new
-  end
-
-  # GET /departments/1/edit
-  def edit
-  end
-
-  # POST /departments
-  # POST /departments.json
-  def create
-    @department = Department.create(department_params)
     respond_with @department
   end
 
-  # PATCH/PUT /departments/1
-  # PATCH/PUT /departments/1.json
+  def new
+    @department = Department.new
+    respond_with @department
+  end
+
+  def edit
+    respond_with @department
+  end
+
+  def create
+    @department = Department.new(department_params)
+    @department.save
+    respond_with @department
+  end
+
   def update
     @department.update(department_params)
     respond_with @department
   end
 
-  # DELETE /departments/1
-  # DELETE /departments/1.json
   def destroy
     @department.destroy
     respond_with @department
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_department
       @department = Department.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def department_params
       params.require(:department).permit(:name)
     end
